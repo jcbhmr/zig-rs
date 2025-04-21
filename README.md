34c9ea87749df546725fa75676088ae7ed33e247
Jacob Hummer <jcbhmr@outlook.com>
2025-04-21 18:52:40 +0000

    saveveveve

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # zig-rs
 ⚡ Zig packaged for Rust
+
+## Installation
+
+```sh
+cargo install zig
+```
+
+## Usage
+
+```sh
+
+```
+
+## Development
--- build.rs
+++ build.rs
@@ -1,7 +1,25 @@
-use std::error::Error;
+use std::{error::Error, process::Command};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    
+    // let status = cc::Build::new()
+    //     .get_compiler()
+    //     .to_command()
+    //     .args([
+    //         "-o",
+    //         "zig-bootstrap/zig/bootstrap",
+    //         "zig-bootstrap/zig/bootstrap.c",
+    //     ])
+    //     .status()?;
+    // if !status.success() {
+    //     return Err(format!("Failed to compile bootstrap.c: {}", status).into());
+    // }
+
+    // let status = Command::new("./bootstrap")
+    //     .current_dir("zig-bootstrap/zig")
+    //     .status()?;
+    // if !status.success() {
+    //     return Err(format!("Failed to run ./bootstrap: {}", status).into());
+    // }
 
     Ok(())
 }
